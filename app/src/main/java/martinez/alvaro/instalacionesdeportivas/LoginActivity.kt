3fcc5767591b8b5a8e63751b06466c27ba9f17c5
AlvaroG2 <alvaromagu2002@gmail.com>
2021-10-22 16:33:31 +0200

    Login Layout Cambios

--- app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
+++ app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
@@ -2,6 +2,8 @@ package martinez.alvaro.instalacionesdeportivas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
