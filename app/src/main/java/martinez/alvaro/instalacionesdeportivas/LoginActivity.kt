764155e58bd178353f05927c06d441a6445eb2ab
AlvaroG2 <alvaromagu2002@gmail.com>
2021-10-23 19:13:35 +0200

    Login Layout events

--- app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
+++ app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
@@ -2,12 +2,31 @@ package martinez.alvaro.instalacionesdeportivas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener { Login(inputEmail.editableText.toString(), inputPassword.editableText.toString()) }
+        txtCreateAccount.setOnClickListener { CreateAccount() }
+        txtForgotPassword.setOnClickListener { RestorePassword() }
     }
+
+    private fun Login(email:String, password:String) {
+        // TODO
+        println("Email: $email, Password: $password")
+    }
+
+    private fun CreateAccount(){
+        // TODO
+    }
+
+    private fun RestorePassword() {
+        // TODO
+    }
+
 }
