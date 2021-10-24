a37feeff12e27e9abf4828a1794cbee3ffb6110c
Alvaro <alvaromagu2002@gmail.com>
2021-10-24 20:15:12 +0200

    EMAIL FORMAT VALIDATION

--- app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
+++ app/src/main/java/martinez/alvaro/instalacionesdeportivas/LoginActivity.kt
@@ -1,8 +1,11 @@
 package martinez.alvaro.instalacionesdeportivas
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.provider.CalendarContract
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -11,22 +14,37 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        btnLogin.setOnClickListener { Login(inputEmail.editableText.toString(), inputPassword.editableText.toString()) }
-        txtCreateAccount.setOnClickListener { CreateAccount() }
-        txtForgotPassword.setOnClickListener { RestorePassword() }
+        btnLogin.setOnClickListener { login(inputEmail.editableText.toString(), inputPassword.editableText.toString()) }
+        txtCreateAccount.setOnClickListener { createAccount() }
+        txtForgotPassword.setOnClickListener { restorePassword() }
     }
 
-    private fun Login(email:String, password:String) {
+    private fun login(email:String?, password:String?) {
         // TODO
-        println("Email: $email, Password: $password")
+        /* EMAIL FORMAT VALIDATION */
+        if(!isValidEmail(email)) {
+            txtEmailError.visibility = View.VISIBLE
+            return
+        }
+        else txtEmailError.visibility = View.GONE
+
+        /* CHECK IF USER IS REGISTERED ON DATABASE */
+        // IF NOT
+            // ASK USER IF WANT TO REGISTER
+            // IF YES GO TO REGISTER PAGE
+            // ELSE DONT DO ANYTHING
+        // ELSE GO TO MAIN LAYOUT
+
     }
 
-    private fun CreateAccount(){
+    private fun createAccount(){
         // TODO
     }
 
-    private fun RestorePassword() {
+    private fun restorePassword() {
         // TODO
     }
+
+    private fun isValidEmail(email:String?):Boolean = !(email === null) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
 }
