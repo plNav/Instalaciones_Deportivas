85656eeff40d2e16783c5c274cca1b48cbe67671
Alvaro Martinez <alvaromagu2002@gmail.com>
2021-11-27 19:20:40 +0100

    Big changes

--- app/src/main/java/martinez/alvaro/instalacionesdeportivas/SigninActivity.kt
+++ app/src/main/java/martinez/alvaro/instalacionesdeportivas/SigninActivity.kt
@@ -1,34 +1,35 @@
 package martinez.alvaro.instalacionesdeportivas
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import martinez.alvaro.instalacionesdeportivas.databinding.ActivityLoginBinding
+import martinez.alvaro.instalacionesdeportivas.databinding.ActivitySigninBinding
 
-class LoginActivity : AppCompatActivity() {
+class SigninActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLoginBinding
+    private lateinit var binding:ActivitySigninBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         auth = Firebase.auth
-        binding.btnLogin.setOnClickListener { login(binding.inputEmail.text.toString(), binding.inputPassword.text.toString()) }
+        if(auth.currentUser != null) loadMainLayout()
+
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSignIn.setOnClickListener { login(binding.inputEmail.text.toString(), binding.inputPassword.text.toString()) }
         binding.txtCreateAccount.setOnClickListener { createAccount() }
         binding.txtForgotPassword.setOnClickListener { restorePassword() }
     }
 
     private fun login(email:String?, password:String?) {
-
         /* CHECK IF USER IS REGISTERED ON DATABASE */
-
         if(email.isNullOrBlank() || password.isNullOrBlank()) return
-
         auth.signInWithEmailAndPassword(email, password).
             addOnCompleteListener(this) {
                 if(it.isSuccessful) loadMainLayout()
@@ -36,21 +37,18 @@ class LoginActivity : AppCompatActivity() {
             }
 
         /* CHECK IF USER IS REGISTERED ON DATABASE */
-
     }
 
     private fun loadMainLayout() {
-        TODO("Not yet implemented")
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        this.startActivity(intent)
     }
 
     private fun createAccount(){
-
-        /* LOAD CREATE ACCOUNT LAYOUT */
-
-            // private fun isValidEmail(email:String?):Boolean = !(email === null) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-
-        /* LOAD CREATE ACCOUNT LAYOUT */
-
+        val intent = Intent(this, SignupActivity::class.java)
+        this.startActivity(intent)
     }
 
     private fun restorePassword() {
---
+++ app/src/main/java/martinez/alvaro/instalacionesdeportivas/SignupActivity.kt
@@ -0,0 +1,101 @@
+package martinez.alvaro.instalacionesdeportivas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import martinez.alvaro.instalacionesdeportivas.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        database = Firebase.firestore
+
+        val opciones = listOf("1.00", "1.50", "2.00", "2.50", "3.00", "3.50", "4.00", "4.50", "5.00")
+
+        binding.spinnerPuntuacion.adapter = ArrayAdapter(this, R.layout.spinner_item_putuacion, opciones)
+
+        binding.btnSignUp.setOnClickListener {
+            try {
+                clearErrors()
+                signUp( binding.inputEmailSignUp.text.toString(),
+                    binding.inputPasswordSignUp.text.toString(),
+                    binding.inputNameSignUp.text.toString() )
+            }catch (ex: NotValidEmail) { setError(binding.inputEmailSignUp, ex.message.toString()) }
+            catch (ex: NotValidPassword) { setError(binding.inputPasswordSignUp, ex.message.toString()) }
+            catch (ex: NotValidName) { setError(binding.inputNameSignUp, ex.message.toString()) }
+        }
+
+    }
+
+    private fun setError(view: EditText, message: String) {
+        view.error = message
+        Toast.makeText(applicationContext, "Credentials error.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun clearErrors() {
+        binding.txtEmailError.visibility = View.GONE
+        binding.txtPasswordError.visibility = View.GONE
+        binding.txtNameError.visibility = View.GONE
+    }
+
+    private fun signUp(email: String, password: String, name: String) {
+
+        if(!checkEmail(email)) throw NotValidEmail("Email is not valid: example@example.com")
+        else if(!checkPassword(password))
+            if(password.length < 6) throw NotValidPassword("Minimum characters: 6.")
+            else throw NotValidPassword("Both passwords must be equals.")
+        else if(name.isBlank()) throw NotValidName("Name cant be blank.")
+        else {
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                val user = hashMapOf(
+                    "email" to email,
+                    "name" to name,
+                    "image" to "gs://instalaciones-deportivas-ddb39.appspot.com/user.png",
+                    "date" to "",
+                    "phone" to "",
+                    "puntuacion" to binding.spinnerPuntuacion.selectedItem.toString()
+                )
+                database.collection("users").add(user).addOnSuccessListener {
+                    Toast.makeText(applicationContext, "User registered.", Toast.LENGTH_SHORT).show()
+                    auth.signOut()
+                    loadSignIn()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext, "Email already in use.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun checkPassword(password: String): Boolean = password.isNotBlank() &&
+                                                           password.length >= 6 &&
+                                                           password == binding.inputPasswordRepeatSignUp.text.toString()
+
+    private fun checkEmail(email: String): Boolean = email.isNotBlank() &&
+                                                     android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    private fun loadSignIn() {
+        val intent = Intent(this, SigninActivity::class.java)
+        this.startActivity(intent)
+        this.finish()
+    }
+
+}
